1b9fb541cdad510e039a8b7e5959975cd2e698da
Sébastien Masset <sebastien.masset@orange.com>
2022-02-18 10:59:28 +0100

    Added LB id and pool IDs to module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "id" {
+  description = "The LB ID"
+  value       = flexibleengine_lb_loadbalancer_v2.loadbalancer.id
+}
+
 output "public_ip" {
   description = "The LB public IP"
   value       = var.bind_eip ? var.eip_addr == null ? flexibleengine_networking_floatingip_v2.loadbalancer_eip[0].address : var.eip_addr : null
@@ -6,4 +11,9 @@ output "public_ip" {
 output "private_ip" {
   description = "The LB private IP"
   value       = flexibleengine_lb_loadbalancer_v2.loadbalancer.vip_address
+}
+
+output "pools" {
+  description = "The LB pools"
+  value       = [for pool in flexibleengine_lb_pool_v2.pools : { id = pool.id, name = pool.name }]
 }
